0b7efd6bce014d1208e59aa61ea14cec6cb68a2d
Ivan Kosenko <kosenko.ivan@gmail.com>
2021-05-11 23:01:46 +0300

    python3 compatibility

--- src/extension.cpp
+++ src/extension.cpp
@@ -37,19 +37,18 @@ struct hasheddictionary {
 inline PyTypeObject *getHashedDictionaryType();
 
 // Get start address of Py_buffer as const char pointer (Python uses void*).
-inline const char *start(const Py_buffer &buffer)
+inline const char* start(const Py_buffer& buffer)
 {
     return reinterpret_cast<const char*>(buffer.buf);
 }
 
 // For consistency of access, this gives the length of a Py_buffer.
-inline std::size_t len(const Py_buffer &buffer)
+inline std::size_t len(const Py_buffer& buffer)
 {
     return buffer.len;
 }
 
-static PyObject *
-simple_encode(Py_buffer &dictionary, Py_buffer &target)
+static PyObject* simple_encode(Py_buffer& dictionary, Py_buffer& target)
 {
     std::string delta;
 
@@ -61,11 +60,10 @@ simple_encode(Py_buffer &dictionary, Py_buffer &target)
     PyBuffer_Release(&target);
     PyBuffer_Release(&dictionary);
 
-    return PyString_FromStringAndSize(delta.data(), delta.size());
+    return PyBytes_FromStringAndSize(delta.data(), delta.size());
 }
 
-static PyObject *
-hashdict_encode(hasheddictionary *hashdict, Py_buffer &target)
+static PyObject* hashdict_encode(hasheddictionary* hashdict, Py_buffer& target)
 {
     HashedDictionary &hd = hashdict->imp;
     std::string delta;
@@ -87,21 +85,18 @@ hashdict_encode(hasheddictionary *hashdict, Py_buffer &target)
 
     PyBuffer_Release(&target);
     Py_DECREF((PyObject*) hashdict);
-    return PyString_FromStringAndSize(delta.data(), delta.size());
+    return PyBytes_FromStringAndSize(delta.data(), delta.size());
 }
 
-static PyObject *
-openvcdiff_encode(PyObject *self, PyObject *args, PyObject *kwargs)
+static PyObject* openvcdiff_encode(PyObject* self, PyObject* args, PyObject *kwargs)
 {
     Py_buffer target;
-    PyObject *dictionary;
+    PyObject* dictionary;
 
-    static const char *keywords[] = { "target", "dictionary", NULL };
+    static const char* keywords[] = { "target", "dictionary", NULL };
 
-    /* The s* format actually enforces a continuous buffer, which is not
-       mentioned in the Python documentation. */
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s*O:encode",
-            const_cast<char**>(keywords), &target, &dictionary)) {
+    /* The s* format actually enforces a continuous buffer, which is not mentioned in the Python documentation. */
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "y*O:encode", const_cast<char**>(keywords), &target, &dictionary)) {
         return NULL;
     }
 
@@ -120,24 +115,20 @@ openvcdiff_encode(PyObject *self, PyObject *args, PyObject *kwargs)
     if (Py_TYPE(dictionary) != getHashedDictionaryType()) {
         PyBuffer_Release(&target);
         Py_DECREF(dictionary);
-        return PyErr_Format(PyExc_TypeError,
-                "encode() argument 2 must be string, buffer or HashedDictionary");
+        return PyErr_Format(PyExc_TypeError, "encode() argument 2 must be buffer or HashedDictionary");
     }
 
-    return hashdict_encode((hasheddictionary*) dictionary, target);
+    return hashdict_encode((hasheddictionary*)dictionary, target);
 }
 
-static PyObject *
-openvcdiff_decode(PyObject *self, PyObject *args, PyObject *kwargs)
+static PyObject* openvcdiff_decode(PyObject* self, PyObject* args, PyObject* kwargs)
 {
     Py_buffer delta, dictionary;
 
-    static const char *keywords[] = { "delta", "dictionary", NULL };
+    static const char* keywords[] = { "delta", "dictionary", NULL };
 
-    /* The s* format actually enforces a continuous buffer, which is not
-       mentioned in the Python documentation. */
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s*s*:decode",
-            const_cast<char**>(keywords), &delta, &dictionary)) {
+    /* The s* format actually enforces a continuous buffer, which is not mentioned in the Python documentation. */
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "y*y*:decode", const_cast<char**>(keywords), &delta, &dictionary)) {
         return NULL;
     }
 
@@ -145,8 +136,7 @@ openvcdiff_decode(PyObject *self, PyObject *args, PyObject *kwargs)
 
     Py_BEGIN_ALLOW_THREADS
 
-    // Too bad, this is one more unneeded copy: The simple interface wants
-    // a string object for the input data...
+    // Too bad, this is one more unneeded copy: The simple interface wants a string object for the input data...
     std::string delta_string(start(delta), len(delta));
 
     VCDiffDecoder decoder;
@@ -157,34 +147,30 @@ openvcdiff_decode(PyObject *self, PyObject *args, PyObject *kwargs)
     PyBuffer_Release(&delta);
     PyBuffer_Release(&dictionary);
 
-    return PyString_FromStringAndSize(target.data(), target.size());
+    return PyBytes_FromStringAndSize(target.data(), target.size());
 }
 
 static PyMethodDef OpenVcdiffMethods[] = {
     {"encode", (PyCFunction) openvcdiff_encode, METH_VARARGS | METH_KEYWORDS,
      "encode(target, dictionary)\n"
-     "Encodes *target* using the data in *dictionary*. Both can be byte "
-     "strings or memory views. Returns a memory view with the delta."},
+     "Encodes *target* using the data in *dictionary*. Both can be byte strings or memory views. Returns a memory view with the delta."},
     {"decode", (PyCFunction) openvcdiff_decode, METH_VARARGS | METH_KEYWORDS,
      "decode(delta, dictionary)\n"
-     "Applies the given *delta* to the *dictionary* data. Again, both "
-     "can be byte strings or memory views. Returns a memory view with the "
-     "reconstructed target."},
+     "Applies the given *delta* to the *dictionary* data. Again, both can be byte strings or memory views. Returns a memory view with the reconstructed target."},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
-
-PyObject *hasheddictionary_new(PyTypeObject *subtype, PyObject *args, PyObject *kwds)
+PyObject* hasheddictionary_new(PyTypeObject* subtype, PyObject* args, PyObject* kwds)
 {
-    Py_buffer dictionary;
-    bool init_okay;
+	Py_buffer dictionary;
 
-    if (!PyArg_ParseTuple(args, "s*:HashedDictionary", &dictionary))
+    if (!PyArg_ParseTuple(args, "y*:HashedDictionary", &dictionary))
         return NULL;
 
-    hasheddictionary *self = (hasheddictionary*) PyType_GenericNew(subtype, args, kwds);
+	bool init_okay;
+    hasheddictionary* self = (hasheddictionary*)PyType_GenericNew(subtype, args, kwds);
     Py_BEGIN_ALLOW_THREADS
-    HashedDictionary *pimp = new (&self->imp) HashedDictionary(start(dictionary), len(dictionary));
+    HashedDictionary* pimp = new (&self->imp) HashedDictionary(start(dictionary), len(dictionary));
     assert(pimp == &self->imp);
     init_okay = pimp->Init();
     Py_END_ALLOW_THREADS
@@ -192,60 +178,57 @@ PyObject *hasheddictionary_new(PyTypeObject *subtype, PyObject *args, PyObject *
         self->imp.~HashedDictionary();
         return PyErr_Format(PyExc_RuntimeError, "HashedDictionary::Init failed.");
     }
-    return (PyObject *) self;
+    return (PyObject*)self;
 }
 
-static void hasheddictionary_dealloc(PyObject *self)
+static void hasheddictionary_dealloc(PyObject* self)
 {
-    hasheddictionary *hdself = (hasheddictionary*) self;
+    hasheddictionary* hdself = (hasheddictionary*)self;
     hdself->imp.~HashedDictionary();
     Py_TYPE(self)->tp_free(self);
 }
 
 
 static PyTypeObject HashedDictionaryType = {
-    PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "openvcdiff.HashedDictionary", /*tp_name*/
-    sizeof(hasheddictionary),  /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    hasheddictionary_dealloc,  /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
-    /* tp_doc */
-    "HashedDictionary for creating multiple datas from the same dictionary data.",
-    0,                         /*tp_traverse*/
-    0,                         /*tp_clear*/
-    0,                         /*tp_richcompare*/
-    0,                         /*tp_weaklistoffset*/
-    0,                         /*tp_iter*/
-    0,                         /*tp_iternext*/
-
-    /* Attribute descriptor and subclassing stuff */
-    0,                         /*tp_methods*/
-    0,                         /*tp_members*/
-    0,                         /*tp_getset*/
-    0,                         /*tp_base*/
-    0,                         /*tp_dict*/
-    0,                         /*tp_descr_get*/
-    0,                         /*tp_descr_set*/
-    0,                         /*tp_dictoffset*/
-    0,                         /*tp_init*/
-    0,                         /*tp_alloc*/
-    hasheddictionary_new,      /*tp_new*/
+	PyVarObject_HEAD_INIT(NULL, 0)
+	"openvcdiff.HashedDictionary",  /* tp_name */
+	sizeof(hasheddictionary),       /* tp_basicsize */
+	0,                              /* tp_itemsize */
+	hasheddictionary_dealloc,       /* tp_dealloc */
+	0,                              /* tp_vectorcall_offset */
+	0,                              /* tp_getattr */
+	0,                              /* tp_setattr */
+	0,                              /* tp_as_async */
+	0,                              /* tp_repr */
+	0,                              /* tp_as_number */
+	0,                              /* tp_as_sequence */
+	0,                              /* tp_as_mapping */
+	0,                              /* tp_hash */
+	0,                              /* tp_call */
+	0,                              /* tp_str */
+	0,                              /* tp_getattro */
+	0,                              /* tp_setattro */
+	0,                              /* tp_as_buffer */
+	Py_TPFLAGS_DEFAULT,             /* tp_flags */
+	/* tp_doc */
+	"HashedDictionary for creating multiple datas from the same dictionary data.",
+	0,                              /* tp_traverse */
+	0,                              /* tp_clear */
+	0,                              /* tp_richcompare */
+	0,                              /* tp_weaklistoffset */
+	0,                              /* tp_iter */
+	0,                              /* tp_iternext */
+	0,                              /* tp_methods */
+	0,                              /* tp_members */
+	0,                              /* tp_getset */
+	0,                              /* tp_base */
+	0,                              /* tp_dict */
+	0,                              /* tp_descr_get */
+	0,                              /* tp_descr_set */
+	0,                              /* tp_dictoffset */
+	0,                              /* tp_init */
+	0,                              /* tp_alloc */
+	hasheddictionary_new,           /* tp_new */
 };
 
 inline PyTypeObject *getHashedDictionaryType()
@@ -253,18 +236,24 @@ inline PyTypeObject *getHashedDictionaryType()
     return &HashedDictionaryType;
 }
 
+static struct PyModuleDef openvcdiffmodule = {
+    PyModuleDef_HEAD_INIT,
+    "openvcdiff",  /* name of module */
+    "Binding for https://github.com/google/open-vcdiff", /* module documentation, may be NULL */
+    -1,       /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+    OpenVcdiffMethods,
+};
 
 PyMODINIT_FUNC
-initopenvcdiff(void)
+PyInit_openvcdiff(void)
 {
-    PyObject *module;
-
     if (PyType_Ready(&HashedDictionaryType) < 0)
-        return;
+        return 0;
 
-    module = Py_InitModule("openvcdiff", OpenVcdiffMethods);
+    PyObject* module = PyModule_Create(&openvcdiffmodule);
     if (module) {
         Py_INCREF(&HashedDictionaryType);
-        PyModule_AddObject(module, "HashedDictionary", (PyObject *) &HashedDictionaryType);
+        PyModule_AddObject(module, "HashedDictionary", (PyObject*)&HashedDictionaryType);
     }
+    return module;
 }
